1edb702ca39c6df1f950ec18c3de5572eb5cc0ec
Syellow <1102137216@gm.kuas.edu.tw>
2016-03-24 08:41:27 +0800

    還沒動態產生selection的選項

--- eSale/Controllers/OrderController.cs
+++ eSale/Controllers/OrderController.cs
@@ -11,6 +11,10 @@ namespace eSale.Controllers
         // GET: Order
         public ActionResult Index()
         {
+            Models.OrderService orderService = new Models.OrderService();
+            var order = orderService.GetOrderById("111");
+            ViewBag.CustId = order.CustId;
+            ViewBag.CustName = order.CustName;
             return View();
         }
     }
---
+++ eSale/Models/OrderService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eSale.Models
+{
+    /// <summary>
+    /// 訂單服務
+    /// </summary>
+    public class OrderService
+    {
+        /// <summary>
+        /// 新增訂單
+        /// </summary>
+        public void InsertOrder()
+        {
+
+        }
+
+        /// <summary>
+        /// 刪除訂單
+        /// </summary>
+        public void DeleteOrderById()
+        {
+
+        }
+
+        /// <summary>
+        /// 更新訂單
+        /// </summary>
+        public void UpdateOrder()
+        {
+
+        }
+
+        /// <summary>
+        /// 取得訂單
+        /// </summary>
+        /// <param name="id">訂單ID</param>
+        /// <returns></returns>
+        public Models.Order GetOrderById(string id)
+        {
+            Models.Order result = new Order();
+            result.CustId = "111";
+            result.CustName = "mis";
+            return result;
+        }
+
+        /// <summary>
+        /// 取得訂單(多筆)
+        /// </summary>
+        /// <returns></returns>
+        public List<Models.Order> GetOrder()
+        {
+            return new List<Order>();
+        }
+    }
+}
